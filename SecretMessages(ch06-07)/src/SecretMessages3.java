41a77845e8560db5e08138c22021ef2fb0b11909
Artemii Petrovskii <artemii.petrovskii@outlook.com>
2019-07-11 23:39:59 +0300

    Chapters: 6-7

---
+++ SecretMessages(ch06-07)/src/SecretMessages3.java
@@ -0,0 +1,51 @@
+import java.util.Scanner; 
+
+public class SecretMessages3 {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		String message = scan.nextLine();
+		String output = "";
+		System.out.println("Enter a secret key (-25 to 25):");
+		int keyVal = Integer.parseInt(scan.nextLine());
+		char key = (char) keyVal;
+		while (keyVal==0) {
+		    System.out.println("Enter a secret key (-25 to 25):");
+		    try {
+		        keyVal = Integer.parseInt(scan.nextLine());
+		    }
+		    catch(Exception ex) { 
+		        System.out.println("Not a valid integer.");
+		    }
+		}		
+		for ( int x = 0; x < message.length(); x++ ) {
+			char input = message.charAt(x);
+			if (input >= 'A' && input <= 'Z') 
+			{ 
+				input += key;
+				if (input > 'Z')
+					input -= 26;
+				if (input < 'A')
+					input += 26;
+			}
+			else if (input >= 'a' && input <= 'z')
+			{ 
+				input += key;
+				if (input > 'z')
+					input -= 26;
+				if (input < 'a')
+					input += 26;
+			}
+			else if (input >= '0' && input <= '9')
+			{
+				input += (keyVal % 10);
+				if (input > '9')
+					input -= 10;
+				if (input < '0')
+					input += 10;
+			}
+			output += input;
+		}
+		System.out.println(output);
+        scan.close();
+    }
+}
